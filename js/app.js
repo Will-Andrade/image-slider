1abd329034cbca3b16eb7b3d6c0a9c97bd764a35
Will-Andrade <willian.santos.andra@outlook.com>
2022-01-23 15:06:35 -0400

    finishes the image slider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+const sliderImgs = document.querySelectorAll('[data-js="slider-item"]');
+const prevSlideBtn = document.querySelector('[data-js="slider-btn-prev"]');
+const nextSlideBtn = document.querySelector('[data-js="slider-btn-next"]');
+
+let sliderIndex = 0;
+
+prevSlideBtn.addEventListener('click', () => {
+    sliderImgs[sliderIndex].classList.remove('slider-item__visible');
+    sliderIndex === 0 ? sliderIndex = sliderImgs.length - 1 : sliderIndex--;
+    sliderImgs[sliderIndex].classList.add('slider-item__visible');
+});
+
+nextSlideBtn.addEventListener('click', () => {
+    sliderImgs[sliderIndex].classList.remove('slider-item__visible');
+    sliderIndex === sliderImgs.length - 1 ? sliderIndex = 0 : sliderIndex++;
+    sliderImgs[sliderIndex].classList.add('slider-item__visible');
+
+});
